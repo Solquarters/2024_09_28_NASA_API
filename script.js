8fa596685f4fdd120db63951bd1a97f27f695be1
Solquarters <rom.kabucov@gmail.com>
2024-09-28 15:19:22 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+///First try dynamik visualization 
+const exoplanet = document.querySelector('.exoplanet');
+const planetData = {
+  sizeFactor: 0.5,
+  temperature: -120,
+};
+
+exoplanet.style.setProperty('--planetSizeFactor', planetData.sizeFactor);
+exoplanet.style.setProperty('--planetTemperature', planetData.temperature);
+///First try dynamik visualization 
